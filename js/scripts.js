0811c180b66c0a92f2a6fcdfccbc14ea899335d0
Nicole Freed <nicole.freed@gmail.com>
2017-06-02 11:47:17 -0700

    implement first (primitive) branching and confirm correct data retrieval from form dropdowns

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,38 @@
 $(document).ready(function() {
   $("#track-selector-form").submit(function(event) {
-    var frontVsBack = $("input#front-vs-back").val();
-    var enterpriseSoftware = $("input#enterprise-software").val();
-    var androidDevelopment= $("input#android-development").val();
-    var evilMicrosoft = $("input#evil-microsoft").val();
-    var wordpressBusinessDevelopment = $("input#wordpress-business-development").val();
-    var dynamicWebApps = $("input#dynamic-web-apps").val();
+    var frontVsBack = $("#front-vs-back").val();
+      console.log(frontVsBack);
+    var enterpriseSoftware = $("#enterprise-software").val();
+      console.log(enterpriseSoftware);
+    var androidDevelopment= $("#android-development").val();
+      console.log(androidDevelopment);
+    var evilMicrosoft = $("#evil-microsoft").val();
+      console.log(evilMicrosoft);
+    var wordpressBusinessDevelopment = $("#wordpress-business-development").val();
+      console.log(wordpressBusinessDevelopment);
+    var dynamicWebApps = $("#dynamic-web-apps").val();
+      console.log(dynamicWebApps);
 
     // branching tree goes here
+    if (frontVsBack === "1" || frontVsBack === "3") {
+      $("#results").show();
+      $(".cssDesign").show();
+    } else {
+      $("#results").show();
+      $(".androidDevelopment").show();
+      $(".evilMicrosoft").show();
+      $(".wordpressBusinessDevelopment").show();
+      $(".dynamicWebApps").show();
+    };
 
-    $(".cssDesign").text(frontVsBack);
-    $(".enterpriseSoftware").text(enterpriseSoftware);
-    $(".androidDevelopment").text(androidDevelopment);
-    $(".evilMicrosoft").text(evilMicrosoft);
-    $(".wordpressBusinessDevelopment").text(wordpressBusinessDevelopment);
-    $(".dynamicWebApps").text(dynamicWebApps);
+    // $(".cssDesign").text(frontVsBack);
+    // $(".enterpriseSoftware").text(enterpriseSoftware);
+    // $(".androidDevelopment").text(androidDevelopment);
+    // $(".evilMicrosoft").text(evilMicrosoft);
+    // $(".wordpressBusinessDevelopment").text(wordpressBusinessDevelopment);
+    // $(".dynamicWebApps").text(dynamicWebApps);
 
-    $("#results").show();
+    // $("#results").show();
 
     event.preventDefault();
   });
