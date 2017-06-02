67573820beb23f3cbaa801d37184190e4dde4af6
Nicole Freed <nicole.freed@gmail.com>
2017-06-02 15:29:47 -0700

    add first name text-entry field and track choice message personalization

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
   $("#track-selector-form").submit(function(event) {
+    var firstName = $("input#first-name").val();
+      // console.log(firstName);
     var frontVsBack = $("#front-vs-back").val();
       // console.log(frontVsBack);
     var enterpriseSoftware = $("#enterprise-software").val();
@@ -17,50 +19,60 @@ $(document).ready(function() {
     if (frontVsBack === "1") {
       $("#results").show();
       $(".cssDesign").show();
+      $(".firstName").text(firstName);
       $(".idealResult").text("CSS/Design");
     } else {
       if (enterpriseSoftware === "Yes"){
         if (androidDevelopment === "Yes" && evilMicrosoft === "No"){
           $("#results").show();
           $(".androidDevelopment").show();
+          $(".firstName").text(firstName);
           $(".idealResult").text("Java/Android");
         } else if (androidDevelopment === "No" && evilMicrosoft === "Yes") {
           $("#results").show();
           $(".evilMicrosoft").show();
+          $(".firstName").text(firstName);
           $(".idealResult").text("C#/.NET");
         } else {
           $("#results").show();
           $(".androidDevelopment").show();
           $(".evilMicrosoft").show();
+          $(".firstName").text(firstName);
           $(".idealResult").text("Java/Android");
         };
       } else {
         if (androidDevelopment === "Yes" && evilMicrosoft === "No"){
           $("#results").show();
           $(".androidDevelopment").show();
+          $(".firstName").text(firstName);
           $(".idealResult").text("Java/Android");
         } else if (androidDevelopment === "No" && evilMicrosoft === "Yes") {
           $("#results").show();
           $(".evilMicrosoft").show();
+          $(".firstName").text(firstName);
           $(".idealResult").text("C#/.NET");
         } else if (androidDevelopment === "Yes" && evilMicrosoft === "Yes"){
           $("#results").show();
           $(".androidDevelopment").show();
           $(".evilMicrosoft").show();
+          $(".firstName").text(firstName);
           $(".idealResult").text("Java/Android");
         } else {
           if (wordpressBusinessDevelopment !== "4" && dynamicWebApps === "No") {
             $("#results").show();
             $(".wordpressBusinessDevelopment").show();
+            $(".firstName").text(firstName);
             $(".idealResult").text("PHP/Drupal");
           } else if (wordpressBusinessDevelopment !== "4" && dynamicWebApps === "Yes") {
             $("#results").show();
             $(".wordpressBusinessDevelopment").show();
             $(".dynamicWebApps").show();
+            $(".firstName").text(firstName);
             $(".idealResult").text("PHP/Drupal");
           } else if (wordpressBusinessDevelopment === "4" && dynamicWebApps === "Yes") {
             $("#results").show();
             $(".dynamicWebApps").show();
+            $(".firstName").text(firstName);
             $(".idealResult").text("Ruby/Rails");
           } else if (wordpressBusinessDevelopment === "4" && dynamicWebApps === "No") {
             $("#results").show();
